a181b4840b08fc3e14b29b8135722467e3d719df
Abbat1 <cozzyyist@gmail.com>
2024-06-24 20:26:19 -0500

    308.4

---
+++ app.js
@@ -0,0 +1,47 @@
+/* The code below with this.side1, this.side 2, and this,.side3, are using the equation a^2+b^2=c^2
+to figure outthe side of the triangle!!*/
+
+/*class Triangle{
+    constructor(side1, side2, side3){
+        this.side1 =side1;
+        this.side2 =side2;
+        this.side3 =side3;
+    }
+    isEquilateralTriangle() {
+        if(
+            this.side1 === this.side2 &&
+            this.side1 === this.side3 &&
+            this.side2 === this.side3
+     ){
+        console.log("Yes This is a EquilateralTriangle");
+     } else {
+        console.log("No This Is a EquilateralTriangle");
+     }
+    }
+    isRightTriangle(){
+    if(this.side1*this.side1 === 
+        this.side2*this.side2 +this.side3 *this.side3 ||
+        this.side2* this.side2 === this.side1* this.side1 +
+        this.side3 *this.side3 ===
+        this.side1* this.side1 +this.side2 * this.side2
+    ){
+}
+}
+}
+
+const aTRiangle = new Triangle(10,9,10);
+aTRiangle.isEquilateralTriangle();*/
+
+/* Part one */
+
+const adventurer = {
+    name: "Bruce wayne",
+    health:10,
+    inventory:["sword", "potion", "artifact"],
+    companion:{
+    name:"leo",
+    type:"cat",
+    companion:"billy",
+    type: "fly",
+ },
+}
